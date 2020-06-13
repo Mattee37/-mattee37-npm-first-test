e0ee5f425734ae01276d8e04f08b587d7a7ec66b
Note <note@note.com>
2020-06-13 19:57:07 -0300

    Adding features

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const axios = require("axios");
 
-const users = async () => {
-  const res = await axios.get(
-    "https://jsonplaceholder.typicode.com/users?_limit=2"
+const users = async (c = 1) => {
+  if (typeof c !== "number") {
+    throw new Error("The value is not a number.");
+  }
+  const r = await axios.get(
+    `https://jsonplaceholder.typicode.com/users?_limit=${c}`
   );
-  return res.data;
+  return r.data;
 };
 
 const PI = 3.1415;
+
+const f = async () => {
+  const r = await users("a");
+  console.log(r);
+};
+f();
 
 module.exports = { users, PI };
