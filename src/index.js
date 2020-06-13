ab4dfdb8406b083ff75eb23ebb0e3020af868030
Note <note@note.com>
2020-06-13 20:06:00 -0300

    fixing new features

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const axios = require("axios");
 
-const users = async (c = 1) => {
-  if (typeof c !== "number") {
-    throw new Error("The value is not a number.");
-  }
-  const r = await axios.get(
-    `https://jsonplaceholder.typicode.com/users?_limit=${c}`
-  );
-  return r.data;
-};
-
 const PI = 3.1415;
 
+const users = async (c = 1) => {
+  if (typeof c === "number") {
+    const r = await axios.get(
+      `https://jsonplaceholder.typicode.com/users?_limit=${c}`
+    );
+    return r.data;
+  } else {
+    throw new Error("The value is not a number.");
+  }
+};
+
 const f = async () => {
-  const r = await users("a");
+  const r = await users(4);
   console.log(r);
 };
 f();
